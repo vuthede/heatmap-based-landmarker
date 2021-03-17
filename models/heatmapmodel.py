a114e3d90b57d1337ac82e161a230be200fa4126
vuthede <dethevu@gmail.com>
2021-03-17 15:12:32 +0700

    (fix) Fix get coordination worng

--- models/heatmapmodel.py
+++ models/heatmapmodel.py
@@ -15,7 +15,7 @@ DEBUG = False
 def heatmap2coord(heatmap, topk=7):
     N, C, H, W = heatmap.shape
     score, index = heatmap.view(N,C,1,-1).topk(topk, dim=-1)
-    coord = torch.cat([index%W, index//W], dim=2)
+    coord = torch.cat([index//W, index%W], dim=2)
     return (coord*F.softmax(score, dim=-1)).sum(-1)
 
 """

7e0649e4ece4ae5b7e56c0a42c4905300e870d93
bml1g12 <ben.lowe.uk@googlemail.com>
2022-07-15 17:09:42 +0100

    fix(models/heatmapmodel.py): torch Resize operation to work with torch==1.12.0

--- models/heatmapmodel.py
+++ models/heatmapmodel.py
@@ -282,7 +282,7 @@ class HeatMapLandmarker(nn.Module):
         self.backbone = mobilenetv2(pretrained=pretrained, model_url=model_url)
         self.heatmap_head = HeatmapHead()
         self.transform = transforms.Compose([
-            transforms.Resize(256, 256),
+            transforms.Resize((256, 256)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])
         ])
